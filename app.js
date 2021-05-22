d713ad04aadf5125463891b3802671b7b9fcbf94
Vangmay <78148958+Vangmay@users.noreply.github.com>
2021-05-22 14:10:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.navlinks');
+const navbar = document.querySelector('.navbar');
+
+burger.addEventListener('click',()=>{
+    nav.classList.toggle('nav-active');
+});
+/*______________________________________________ */
