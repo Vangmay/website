db465399cf933b233f8eb7a7297fa05f20adace1
Vangmay Sachan <78148958+Vangmay@users.noreply.github.com>
2021-05-25 17:32:28 +0530

    Add files via upload

---
+++ Posts/posts.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.navlinks');
+const navbar = document.querySelector('.navbar');
+
+
+
+burger.addEventListener('click',()=>{
+    nav.classList.toggle('nav-active');
+});
--- Projects/projects.js
+++ Projects/projects.js
@@ -1,8 +1,11 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.navlinks');
-const project_card = document.querySelector('.project-cards');
+const project = document.querySelector('.project');
+const navbar = document.querySelector('.navbar');
 
-
+const tl = new TimelineMax();
+tl.fromTo(navbar,0.8,{opacity:0,x:-30},{opacity:1,x:0,ease:Power2.easeIn})
+.fromTo(project,0.5,{opacity:0,y:-20},{opacity:1,y:0,ease:Power1.easeIn})
 
 
 burger.addEventListener('click',()=>{
