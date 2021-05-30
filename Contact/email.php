8b54494ab05740e100bf31f16b996e0795681321
Vangmay Sachan <78148958+Vangmay@users.noreply.github.com>
2021-05-30 19:12:23 +0530

    Delete email.php

--- Contact/email.php
+++
@@ -1,5 +0,0 @@
-
-if ( isset( $_POST['submit'] ) ) {  
-    $firstname = $_POST['name']; 
-    $lastname = $_POST['email']; 
-    echo '<h3>Form POST Method</h3>'; echo 'Your name is ' . $lastname . ' ' . $firstname; exit; } 
