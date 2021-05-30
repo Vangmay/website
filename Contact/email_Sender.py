1fbe9a70c1e98c30411c6d3c17e430d1f824dd1d
Vangmay Sachan <78148958+Vangmay@users.noreply.github.com>
2021-05-30 16:24:34 +0530

    Add files via upload

--- Contact/email_Sender.py
+++ Contact/email_Sender.py
@@ -1,1 +1,62 @@
-print('hello wolrd')
+from email import message
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVICE_ACCOUNT_FILE = 'Contact/keys.json'
+
+credentials = None
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1j5L46F8Js0HsQ4-VmHYqg7EMONcT7DLawm5_YXaRxdk'
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+service = build('sheets', 'v4', credentials=credentials)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range="Sheet1!A1:B30").execute()
+values = result.get('values', [])
+
+emails = []
+names = []
+for i in range(1,len(values)):
+    names.append(values[i][0])
+    emails.append(values[i][1])
+
+print (names)
+print (emails)
+#sending emails
+import smtplib
+# set up the SMTP server
+s = smtplib.SMTP(host='smpt.gmail.com', port=465)
+s.starttls()
+s.login(MY_ADDRESS, PASSWORD)
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+message = "You have been reached out by Vangmay's very handy bot who exists so he can send emails.I am exited to know the reason you wanted to contact me!"
+msg = MIMEMultipart()
+msg['From'] = MY_ADDRESS
+msg['To'] = email
+msg['Subject'] = "Hello there!"
+msg.attach(message,'plain')
+s.send_message(msg)
+del msg
+#sending emails
+
+#to remove data
+#aoa = [["",""],["",""]]
+#request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,range="Sheet1!A2", valueInputOption = "USER_ENTERED",body={"values":aoa}).execute()
+#to remove data
