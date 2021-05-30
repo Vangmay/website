1748e295f3e6dfc5e553d8784ec92368a0753ee9
Vangmay Sachan <78148958+Vangmay@users.noreply.github.com>
2021-05-30 15:00:13 +0530

    Add files via upload

--- Contact/email_Sender.py
+++ Contact/email_Sender.py
@@ -1,7 +1,1 @@
-import json
-import urllib3
-url = 'http://127.0.0.1:5500/Contact/contact.html'
-http = urllib3.PoolManager()
-response = http.request('POST',url,fields={'name':"email"})
-email_data = json.loads(response.data.decode('utf-8'))
-print(email_data)
+print('hello wolrd')
