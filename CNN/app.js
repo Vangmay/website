6c336b4baca94ca140de4fcd4b2945dd088efe02
Vangmay Sachan <78148958+Vangmay@users.noreply.github.com>
2021-07-02 12:45:04 +0530

    Add files via upload

---
+++ CNN/app.js
@@ -0,0 +1,39 @@
+Dropzone.autoDiscover = false;
+pred = document.querySelector('.pred')
+
+
+function init() {
+    
+    
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        uploadMultiple:false,
+        maxFiles: 1,
+        addRemoveLinks: true,
+        //dictDefaultMessage:'<span class="span"><h4 class="uploadBTN">Upload</h4></span>' ,
+        autoProcessQueue: false,
+        //change the previewTemplate to use Bootstrap progress bars
+        previewTemplate:'<div class="dz-preview dz-file-preview"> <div class="dz-details"> <div class="dz-filename"><span data-dz-name></span></div> <div class="dz-size" data-dz-size></div> </div> </div>'
+    });
+
+    dz.on('addedfile',function(){
+        if(dz.files[1]!=null){
+            dz.removeFile(dz.files[0])
+        }
+    });
+    dz.on('complete',function(file){
+        let imageData = file.dataURL;
+
+        var url = 'https://cat-breed-server.herokuapp.com/classify_cat';
+
+        $.post(url, {
+            image_data: imageData
+        },function(data,status){
+            pred.innerHTML = data;
+        });
+    });
+    $('#submitBtn').on('click',function(e){
+        dz.processQueue()
+    });
+}
+init()
