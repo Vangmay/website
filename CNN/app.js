dbf03a4c1e4f55f497ca158a4553b5a4b4ba17d2
Vangmay Sachan <78148958+Vangmay@users.noreply.github.com>
2021-07-03 15:06:27 +0530

    Update app.js

--- CNN/app.js
+++ CNN/app.js
@@ -24,7 +24,7 @@ function init() {
     dz.on('complete',function(file){
         let imageData = file.dataURL;
 
-        var url = 'http://127.0.0.1:5000/classify_cat';
+        var url = 'https://cat-breed-classifier.herokuapp.com/classify_cat';
 
         $.post(url, {
             image_data: imageData
